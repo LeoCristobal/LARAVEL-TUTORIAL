a16bb59213e8bace71c50640b888c0e42791525a
LeoCristobal <leocristobal108@gmail.com>
2025-09-14 08:36:54 +0800

    Day 5

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,6 +1,6 @@
 @props(['active' => false])
 <a
-    class="{{ $active ? 'bg-gray-900 text-white rounded-md px-3 py-2' : 'text-gray-300 hover:bg-white/5 hover:text-white' }}"
+    class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2"
     aria-current="{{ $active ? 'page' : 'false'}}"
     {{ $attributes }}>
     {{ $slot }}
