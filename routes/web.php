79dcd6258627a3e6d41bd77d4ddcbfb590803fe9
LeoCristobal <leocristobal108@gmail.com>
2025-09-27 14:46:27 +0800

    Day 15 and 16

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,30 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $job = Job::with('employer')->cursorPaginate(5);
-    return view('jobs', [
+    $job = Job::with('employer')->latest()->simplePaginate(10);
+    return view('jobs.index', [
         'jobs' => $job
     ]);
 });
 
+Route::post('/jobs', function () {
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+Route::get('/create-job', function() {
+    return view('jobs.create-job');
+});
+
+
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
