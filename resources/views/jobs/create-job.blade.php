6fe0cc4a68a414d503756ac81cbf92cc7e64bd94
LeoCristobal <leocristobal108@gmail.com>
2025-10-01 16:09:03 +0800

    Day 21

--- resources/views/jobs/create-job.blade.php
+++ resources/views/jobs/create-job.blade.php
@@ -12,46 +12,32 @@
                     you share.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                    <x-form-field>
+                        <x-form-label for="title"> Title </x-form-label>
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">
-                                </div>
-                                <input id="title" type="text" name="title" placeholder="Leo"
-                                    class="block min-w-0 grow bg-white py-1.5 px-3 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" required/>
-                            </div>
+                            <x-form-input id="title" type="text" name="title" placeholder="Leo" required/>
+
+                            <x-form-error name="title"/>
                         </div>
-                        @error('title')
-                            <p class="text-s text-red-500 font-semibold mt-1"> {{ $message }}</p>
-                        @enderror
-                    </div>
+                    </x-form-field>
                 </div>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                    <x-form-field>
+                        <x-form-label for="salary"> Salary </x-form-label>
+
                         <div class="mt-2">
-                            <div
-                                class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <div class="shrink-0 select-none text-base text-gray-500 sm:text-sm/6">
-                                </div>
-                                <input id="salary" type="text" name="salary" placeholder="$10,000"
-                                    class="block min-w-0 grow bg-white py-1.5 px-3 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" required/>
-                            </div>
+                            <x-form-input id="salary" type="text" name="salary" placeholder="$10,000" required/>
+                            <x-form-error name="salary" />
                         </div>
-                        @error('salary')
-                            <p class="text-s text-red-500 font-semibold mt-1"> {{ $message }}</p>
-                        @enderror
-                    </div>
+                    </x-form-field>
                 </div>
             </div>
         </div>
+
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <x-form-button> Save </x-form-button>
         </div>
     </form>
 
